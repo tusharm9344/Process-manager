dba63fbdec31406ac367f27598eaca8b19c0fe60
tusharm9344 <tushar.kakrola@gmail.com>
2026-02-03 00:23:56 +0530

    Performed report file destination command

--- organize.sh
+++ organize.sh
@@ -1,34 +1,36 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 set -Eeuo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 REPORT_FILE="$SCRIPT_DIR/report.log"
 
-echo "---------------------"  >> "$REPORT_FILE"
+echo "---------------------" >> "$REPORT_FILE"
 
-echo "Top 5 processes are :" >> 
+echo "Top 5 processes are :" >> "$REPORT_FILE"
 
-ps aux | sort -nrk 3 |head -5 || true
+ps aux | sort -nrk 3 | head -5 >> "$REPORT_FILE" || true
 
 echo "Enter process to search ex (Chrome,Whats app, etc.)"
 
 read PROCESS_NAME
 
-ps aux | grep "$PROCESS_NAME" | grep -v grep || true
+ps aux | grep "$PROCESS_NAME" | grep -v grep >> "$REPORT_FILE" || true
 
 echo "Want to kill a process? (yes/no)"
 
 read VALUE_
 
-if [ "$VALUE_" = "yes" ]; then 
-echo "Enter PID"
-read PID 
-kill "$PID"
-else 
-echo "No process killed "
+if [ "$VALUE_" = "yes" ]; then
+    echo "Enter PID"
+    read PID
+    kill "$PID"
+    echo "Process killed $PID" >> "$REPORT_FILE"
+else
+    echo "No process killed "
 fi
-echo "---------------"
 
-echo "process manager finished"
+echo "---------------" >> "$REPORT_FILE"
+
+echo "process manager finished" >> "$REPORT_FILE"
 
